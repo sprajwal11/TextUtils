15ac83321f553e35b0255f8d109b0c8385c12d91
sprajwal11 <sharmaprajwal11@gmail.com>
2022-08-06 01:43:46 +0530

    added clear text button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,11 @@ export default function
         let newText=text.toLowerCase();
         setText(newText);
     } 
+    const toClearText=()=>{
+        // console.log("Uppercase was clicked "+text);
+        let newText='';
+        setText(newText);
+    } 
     const handleOnChange=(event)=>{
         // console.log("Handled on change");
         setText(event.target.value);
@@ -22,6 +27,7 @@ export default function
     //setText("Write here");
 
      return (
+        <>
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
@@ -29,6 +35,15 @@ export default function
         </div>
         <button className="btn btn-primary" onClick={toUpperCase}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={toLowerCase}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={toClearText}>Clear Text</button>
     </div>
+    <div className="container my-2">
+        <h1>Your text summary</h1>
+        <p>{text.split(' ').length} words and {text.length} characters</p>
+        <p>It will take around <b>{0.008*text.split(' ').length} minutes</b> to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
